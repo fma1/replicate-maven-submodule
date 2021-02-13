527ace68e1af6d7a1fb4343dc62425656db79e12
Fred Ma <fredma1234@gmail.com>
2021-02-13 18:07:51 -0500

    Added replicate-submod.rb and got functionality working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # replicate-maven-submodule
 Replicates a maven sub module
+
+`ruby replicate-submod.rb <oldModule> <newModule>`
+
+This script assumes oldModule is the directory name and the module name.
---
+++ replicate-submod.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+#require 'optparse'
+require 'fileutils'
+
+#options = {}
+#
+#optparse = OptionParser.new do|opts|
+#  opts.banner = "Usage: optparse1.rb [options] file1 file2 ..."
+#  oldModule = nil
+#  newModule = nil
+#  opts.on('-m', '--module', 'Specify module name') do |module|
+#    oldModule = module
+#  end
+#  opts.on('-n', '--new_module', 'Specify new module name') do |newModule|
+#    newModule = newName 
+#  end
+#  opts.on('-h', '--help', 'Display this screen' ) do
+#    puts opts
+#    exit
+#  end
+#end
+#
+#optparse.parse!
+
+oldModule = ARGV[0]
+newModule = ARGV[1]
+
+if oldModule.nil?
+  puts('Old module name is nil')
+  exit
+end
+
+if newModule.nil?
+  puts('New module name is nil')
+  exit
+end
+
+if !Dir.exists?(newModule)
+  puts "Old module directory does not exist"
+end
+
+puts "Copying " + oldModule + " to " + newModule + "..."
+FileUtils.cp_r oldModule, newModule 
+puts "Deleting target folder..."
+targetFolder = newModule + "/target"
+FileUtils.rm_rf(targetFolder)
+
+# Not suitable for production code as data loss can occur
+puts "Replacing old module name in pom.xml..."
+pomFile = newModule + "/pom.xml"
+text = File.read(pomFile)
+new_contents = text.gsub(oldModule, newModule)
+File.open(pomFile, "w") {|file| file.puts new_contents }
